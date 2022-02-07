60f2d9788c8d0a6be701f5ba731c08a8e60898fe
xingyu-Lu <2273466984@qq.com>
2022-02-08 00:15:55 +0800

    update

--- dist/assets/wlzw.58c94ea2.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.ea1bfd5e.js";import{c as r,d as c,e as n}from"./vendor.3ad3b1c7.js";const t={};function s(a,_){const e=r("el-empty");return c(),n(e,{description:"\u5F85\u66F4\u65B0"})}var m=o(t,[["render",s]]);export{m as default};
