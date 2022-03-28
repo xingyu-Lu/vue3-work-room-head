3bb762c1a8b98eb89d35114180f99acd4ea3ed5c
xingyu-Lu <2273466984@qq.com>
2022-03-28 15:54:22 +0800

    update

--- dist/assets/wlzw.0bfcf0d7.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.af27a8db.js";import{c as r,d as c,e as n}from"./vendor.d1c66501.js";const t={};function s(a,_){const e=r("el-empty");return c(),n(e,{description:"\u5F85\u66F4\u65B0"})}var m=o(t,[["render",s]]);export{m as default};
